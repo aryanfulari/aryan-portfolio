2ed964a7e7e172b01bd67b6f846c42bbe9c0bff9
Aryan F <aryanfulari@gmail.com>
2026-09-07 18:53:20 +0530

    add hero landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import HeroLanding from "@/components/hero-landing";
 import { ThemeToggleButton } from "@/components/theme-toggle-button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
-      <ThemeToggleButton variant="circle" start="top-right" />
+    <main className="relative">
+      <div className="absolute top-6 right-6 z-50">
+        <ThemeToggleButton variant="circle" start="top-right" />
+      </div>
+      <HeroLanding />
     </main>
   );
 }
